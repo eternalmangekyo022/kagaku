371cc233d3e3e6dd0ad6ec4d8eded05af1c40c28
eternalmangekyo022 <tibcso0322@gmail.com>
2023-03-03 16:38:04 +0100

    init

---
+++ src/app.d.ts
@@ -0,0 +1,33 @@
+// See https://kit.svelte.dev/docs/types#app
+// for information about these interfaces
+declare global {
+	namespace App {
+		// interface Error {}
+		// interface Locals {}
+		// interface PageData {}
+		// interface Platform {}
+	}
+	type ChemicalElement = {
+		atomicMass: string
+		atomicNumber: number
+		atomicRadius: number
+		boilingPoint: number
+		bondingType: string
+		cpkHexColor: string
+		density: number
+		electronAffinity: number
+		electronConfiguration: string
+		groupBlock: string
+		ionRadius: string
+		ionozationEnergy: number
+		meltingPoint: number
+		name: string
+		oxidationStates: string
+		standardState: string
+		symbol: string
+		vanDerWaalsRadius: number
+		yearDiscovered: number
+	}
+}
+
+export {};
