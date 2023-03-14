84469251fa0c2bc76a43ee1b774ca463c79a5e70
eternalmangekyo022 <tibcso0322@gmail.com>
2023-03-14 20:57:31 +0100

    added switch

--- src/components/Visualizer.svelte
+++ src/components/Visualizer.svelte
@@ -1,12 +1,19 @@
 <script lang='ts'>
-    const tabs: Tab[] = [{ name: 'Elements', elements: [2323] }, { name: 'Bonds', elements: [] }];
+    import Switch from "./Switch.svelte";
+    import { elements } from "../store";
+    import { writable, type Writable } from "svelte/store";
 
-    let tabIndex = 0;
+    const active: Writable<boolean> = writable(false);
+    const tabs: Tab[] = [{ name: 'Elements', elements: $elements }, { name: 'Bonds', elements: [] }];
+
 </script>
 
 <div class='h-full w-full'>
-    <div class='h-full w-[10%] resize-x overflow-x-auto min-w-[5%] max-w-[35%] border-r-2'>
-        <div class='w-full h-[6%] border-b-2 flex justify-center items-center'>Bonds</div>
-        <div></div>
+    <div class='h-full w-[15%] resize-x overflow-x-auto min-w-[12%] max-w-[35%] border-r-2'>
+        <div class='w-full h-[6%] border-b-2 flex justify-center items-center gap-10'>
+            <span>Elements</span>
+            <Switch {active} className='w-[2.5rem] h-[1rem] cursor-pointer border-2 border-black'/>
+            <span>Bonds</span>
+        </div>
     </div>
 </div>
